a33c22c956d8b3109a9f46aabd8ea434f40a5989
Kirby Cool <kirbydcool@gmail.com>
2021-01-03 19:08:40 -0800

    Add setup script.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ln -s $DOTFILES_DIR/.alacritty.yml ~/.alacritty.yml
+ln -s $DOTFILES_DIR/.editorconfig ~/.editorconfig
+ln -s $DOTFILES_DIR/.gitconfig ~/.gitconfig
+ln -s $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
+
+ln -s $DOTFILES_DIR/nvim/* ~/.config/nvim/
+
+ln -s $DOTFILES_DIR/fish/* ~/.config/fish/
