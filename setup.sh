c5e9770bd5a253da81a4c0cbb67a9be6eac9f2f8
Kirby Cool <kirbydcool@gmail.com>
2021-03-29 16:06:54 -0700

    [starship] Add starship config

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ln -s $DOTFILES_DIR/.alacritty.yml ~/.alacritty.yml
 ln -s $DOTFILES_DIR/.editorconfig ~/.editorconfig
+ln -s $DOTFILES_DIR/starship.toml ~/.config/starship.toml
 ln -s $DOTFILES_DIR/.gitconfig ~/.gitconfig
 ln -s $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
 
