efa77a7dfb100e00deeded4955ba1c364a82cb5e
Kirby Cool <kirbydcool@gmail.com>
2024-07-18 10:10:23 -0700

    [alacritty] Convert config to toml

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -s $DOTFILES_DIR/.alacritty.yml ~/.alacritty.yml
 ln -s $DOTFILES_DIR/.editorconfig ~/.editorconfig
 ln -s $DOTFILES_DIR/starship.toml ~/.config/starship.toml
 ln -s $DOTFILES_DIR/.gitconfig ~/.gitconfig
@@ -13,3 +12,7 @@ ln -s $DOTFILES_DIR/nvim/* ~/.config/nvim/
 ln -s $DOTFILES_DIR/fish/* ~/.config/fish/
 
 ln -s $DOTFILES_DIR/zellij/* ~/.config/zellij/
+
+ln -s $DOTFILES_DIR/alacritty/.alacritty.toml ~/.alacritty.toml
+mkdir -p ~/.config/alacritty
+ln -s $DOTFILES_DIR/alacritty/* ~/.config/alacritty/
